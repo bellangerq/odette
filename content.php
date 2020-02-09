ccf344eb1ff5cf6a435e925569f23f499d38b17e
Quentin Bellanger <hello@quentin-bellanger.com>
2020-02-10 00:23:02 +0100

    Get post author

--- content.php
+++ content.php
@@ -1,10 +1,16 @@
-<div class="blog-post">
+<article class="blog-post">
   <?php if ( is_single() ) { ?>
-    <h2 class="blog-post-title"><?php the_title() ?></h2>
+    <h2 class="blog-post-title"><?php echo get_the_title() ?></h2>
   <?php } else { ?>
-    <h2 class="blog-post-title"><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
+    <h2 class="blog-post-title"><a href="<?php echo get_the_permalink() ?>"><?php echo get_the_title() ?></a></h2>
   <?php } ?>
-	<span><time><?php the_date() ?></time> by <?php the_author() ?></span>
+	<span>
+    <time><?php echo get_the_date() ?></time> by <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" title="<?php echo esc_attr( get_the_author() ); ?>"><?php the_author(); ?></a>
+  </span>
 
-  <?php the_content() ?>
-</div>
+  <?php
+    if ( is_single() ) {
+      echo get_the_content();
+    }
+  ?>
+</article>
