f302a911f1f14ddb3dfa5ce01c4306b931b2481d
Quentin Bellanger <hello@quentin-bellanger.com>
2020-02-11 22:25:16 +0100

    Load main stylesheet and add css reset

--- functions.php
+++ functions.php
@@ -1,7 +1,9 @@
 <?php
-function register_header_menu() {
-  register_nav_menu( 'header', __( 'Header', 'Menu located below site title.' ) );
-}
+  function register_header_menu() {
+    register_nav_menu( 'header', __( 'Header', 'Menu located below site title.' ) );
+  }
 
-add_action( 'after_setup_theme', 'register_header_menu' );
+  add_action( 'after_setup_theme', 'register_header_menu' );
+
+  wp_enqueue_style( 'style', get_stylesheet_uri() );
 ?>
