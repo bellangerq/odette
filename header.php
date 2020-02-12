92e15108a9f53bb8d0bf8c09aae05ead4c8cc5b0
Quentin Bellanger <hello@quentin-bellanger.com>
2020-02-12 23:53:23 +0100

    Fixed position footer and header

--- header.php
+++ header.php
@@ -3,15 +3,12 @@
   <body>
     <header class="site-header">
       <a href="#main">Skip to content</a>
-      <div>
-        <a href="<?php echo get_option('home'); ?>">
-          <?php bloginfo('name');?>
-        </a>
-        <?php if ( bloginfo('description') ) { ?>
-          <span><?php bloginfo('description'); ?></span>
-        <?php } ?>
-      </div>
-
+      <a href="<?php echo get_option('home'); ?>">
+        <?php bloginfo('name');?>
+      </a>
+      <?php if ( bloginfo('description') ) { ?>
+        <p><?php bloginfo('description'); ?></p>
+      <?php } ?>
       <?php
       if ( has_nav_menu( 'header' ) ) {
         echo wp_nav_menu('header');
