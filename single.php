ef31da5cff7fe5955361a8443dea809cd87a3438
Quentin Bellanger <hello@quentin-bellanger.com>
2020-03-20 23:21:51 +0100

    Style post content

--- single.php
+++ single.php
@@ -5,14 +5,14 @@
     while (have_posts()) :
       the_post(); ?>
 
-      <header class="post-header">
+      <header class="content-header">
         <h1><?php echo get_the_title() ?></h1>
-        <span class="post-metadata">
+        <span class="content-metadata">
           <time><?php the_date() ?></time> by <?php the_author(); ?>
         </span>
       </header>
 
-      <div class="post-content">
+      <div class="content-body">
         <?php the_content(); ?>
       </div>
 
