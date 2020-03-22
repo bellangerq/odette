537b52923de1e2d2e170f5934bf96cb73f4bfb21
Quentin Bellanger <hello@quentin-bellanger.com>
2020-03-22 22:22:44 +0100

    Add page tempalte

---
+++ page.php
@@ -0,0 +1,25 @@
+<?php get_header(); ?>
+
+<article class="post">
+  <?php
+  while (have_posts()) :
+    the_post(); ?>
+
+    <header class="content-header">
+      <h1><?php echo get_the_title() ?></h1>
+      <span class="content-metadata">
+        By <?php the_author(); ?>
+      </span>
+    </header>
+
+    <div class="content-body">
+      <?php the_content(); ?>
+    </div>
+
+  <?php endwhile; ?>
+</article>
+</main>
+
+<?php get_footer(); ?>
+
+</html>
