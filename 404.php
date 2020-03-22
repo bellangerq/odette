6655119c0a44e67e10fa89221e67341b05701daa
Quentin Bellanger <hello@quentin-bellanger.com>
2020-03-22 22:50:00 +0100

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<article class="post">
+  <header class="content-header">
+    <h1>Page not found</h1>
+  </header>
+
+  <div class="content-body">
+    <p>The page does not exist or has been deleted.</p>
+    <p><a href="#">Back to the homepage.</a></p>
+  </div>
+</article>
+</main>
+
+<?php get_footer(); ?>
+
+</html>
